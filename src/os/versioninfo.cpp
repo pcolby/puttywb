ea4ead291801bd3b6190dc14399a46253b1caf12
Paul Colby <git@colby.id.au>
2014-03-22 11:42:42 +1100

    Added GPL3 license.

--- src/os/versioninfo.cpp
+++ src/os/versioninfo.cpp
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2011-2014 Paul Colby
+ *
+ *  This file is part of PuTTY Workbench (puttywb).
+ *
+ *  PuTTY Workbench is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PuTTY Workbench is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with PuTTY Workbench.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "versioninfo.h"
 
 #ifdef Q_OS_WIN // VersionInfo is a Windows-only class.
